8d329cd9d0c1eefd7f6ad09a6a3a071ae47216c5
ValerioPerrone <valerio.perrone@carloalberto.org>
2015-02-24 10:28:15 +0000

    Multivariate sample in pure R

---
+++ R/GPMH.R
@@ -0,0 +1,46 @@
+#Generalised MH algorithm
+GPMH<-function (target,
+              kernel,  # to draw from the distribution of mean = x
+              dkernel, # to evaluate in y the distribution of mean = x 
+              init.state,
+              n,
+              N=8)  # points to "parallely" draw from the proposal at each iteration
+  
+{
+  d <- length(init.state)
+  I <- 1 
+  X <- matrix(NA, nrow = n*(N + 1), ncol = d)
+  X[1, ] <- init.state
+  Y <- matrix(NA, nrow = N+1, ncol = d)
+  Y[1,] <- X[1,]
+  for(j in 2:(N+1)){
+    Y[j,]<-kernel(X[1, ]) # generate N new points from the proposal
+  }
+  
+  K<-numeric(N+1)
+  A<-numeric(N+1) 
+  
+  for (i in 1:n) { 
+      for(j in 1:(N+1)){
+        for (k in 1:(N+1)) {
+          K[k]<-dkernel(Y[j,],Y[k,])  
+        }
+        A[j]<- prod(K[-j])*target(Y[j,]) # likelihood of each of the N+1 points
+      }
+    
+    # sample from the N+1 points to obtain the new N+1 MCMC samples 
+    
+    X[((i-1)*N+i):(i*(N + 1)),]<-Y[sample(seq(1:(N+1)),replace=TRUE,prob=A),]
+    
+    # Now we set Y for the next iteration:
+    I<-sample(((i-1)*N+i):(i*(N + 1)),1)  # draw uniformly the index I...
+    Y[1,]<-X[I, ] # ...of the X[I,] that will generate N new points
+    for(j in 2:(N+1)){
+      Y[j,]<-kernel(X[I, ]) # generate N new points
+    }
+  }
+
+  output <- list(x = X[-1, ], n = n, call = match.call())
+  class(output) <- "GPMH"
+  return(output)
+}
