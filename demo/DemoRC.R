968d19e8282fd156b90df5ec15c0a4012f142260
Ale Avalos <aleavalosp@gmail.com>
2015-02-25 20:35:37 +0000

    Demo

---
+++ demo/DemoRC.R
@@ -0,0 +1,12 @@
+######rcGMH######
+###R/C
+xrcGMH<-rcGPMH(target=function(x) {0.2*dnorm(x,12,1)+0.3*dnorm(x,8,1)+0.4*dnorm(x,4,1)+0.6*dnorm(x,15,1)+0.3*dnorm(x,20,1)},
+              kernel=function(x) {rnorm(n=1, x)},
+              dkernel=function(x,y) {dnorm(y, x)},
+              init.state=5,
+              n=100000,
+              N=8)
+
+plot(xrcGMH$x)
+summary(xrcGMH$x)
+
