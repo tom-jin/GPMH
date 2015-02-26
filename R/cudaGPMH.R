d575c7635c376379d7d65c4c070f3152e8c03bc4
Tom Jin <tom@jin.me.uk>
2015-02-26 23:48:42 +0000

    Correct allocation size to sample.

--- R/cudaGPMH.R
+++ R/cudaGPMH.R
@@ -1,4 +1,4 @@
 #' @export
 cudaGPMH <- function(target, rkernel, dkernel, init, n, N) 
-  .C("cudaGPMH", as.double(rep(0.0, n)), as.integer(0), as.integer(0), as.integer(0), as.double(init), as.integer(n), as.integer(N))[[1]]
+  .C("cudaGPMH", as.double(rep(0.0, 2*n)), as.integer(0), as.integer(0), as.integer(0), as.double(init), as.integer(n), as.integer(N))[[1]]
 
